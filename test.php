6d1aff4f91e8d5be5c58ba538bf90bfcf8be168c
Md. Fahim Hossain <alifhossain174@gmail.com>
2021-09-11 14:26:31 +0600

    Updating project

--- test.php
+++ test.php
@@ -2,6 +2,18 @@
 
     $curl = curl_init();
 
+    $script = '<?php echo "Hello"; ?>';
+
+    $arr = array(
+        "clientId" => "39882eb0e8a01aac131f9dfdb89f0bef", 
+        "clientSecret" => "85e310de734661111fa1d32a469d3b80d9a864ef548a2c704e68b768ec3a4288", 
+        "script" => $script, 
+        "language" => "php", 
+        "versionIndex" => "0"
+    );
+    
+    $data = json_encode($arr);
+
     curl_setopt_array($curl, array(
         CURLOPT_URL => 'https://api.jdoodle.com/v1/execute',
         CURLOPT_RETURNTRANSFER => true,
@@ -11,13 +23,7 @@
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => '{
-            "clientId": "39882eb0e8a01aac131f9dfdb89f0bef",
-            "clientSecret": "85e310de734661111fa1d32a469d3b80d9a864ef548a2c704e68b768ec3a4288",
-            "script": "<?php echo 123; ?>",
-            "language": "php",
-            "versionIndex": "0"
-        }',
+        CURLOPT_POSTFIELDS => $data,
         CURLOPT_HTTPHEADER => array(
             'Content-Type: application/json'
         ),
