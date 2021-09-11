2f43d2d6a0d0528018bc5a93a12d9825e2a562a7
Md. Fahim Hossain <alifhossain174@gmail.com>
2021-09-11 12:16:37 +0600

    first commit

---
+++ test.php
@@ -0,0 +1,31 @@
+<?php
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.jdoodle.com/v1/execute',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => '{
+            "clientId": "39882eb0e8a01aac131f9dfdb89f0bef",
+            "clientSecret": "85e310de734661111fa1d32a469d3b80d9a864ef548a2c704e68b768ec3a4288",
+            "script": "<?php echo 123; ?>",
+            "language": "php",
+            "versionIndex": "0"
+        }',
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
+
+?>
